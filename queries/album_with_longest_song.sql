99e20ab5e86d03d5bc230aa30e734840742f7597
Jan Stanek <jan.stanek@upol.cz>
2020-02-03 19:38:14 +0100

    uprava formatovani sql

--- queries/album_with_longest_song.sql
+++ queries/album_with_longest_song.sql
@@ -3,14 +3,14 @@
 -- doplnen index na skladba.delka
 
 SELECT
-interpret.nazev AS nazev_interpreta,
-album.nazev AS nazev_alba,
-album.datum_vydani,
-skladba.delka AS delka_skladby
-FROM album
-INNER JOIN album_skladba ON album_skladba.id_album=album.id_album
-INNER JOIN skladba ON skladba.id_skladba=album_skladba.id_skladba
-INNER JOIN album_interpret ON album_interpret.id_album=album.id_album
-INNER JOIN interpret ON interpret.id_interpret=album_interpret.id_interpret
+`interpret`.`nazev` AS `nazev_interpreta`,
+`album`.`nazev` AS `nazev_alba`,
+`album`.`datum_vydani`,
+`skladba`.`delka` AS `delka_skladby`
+FROM `album`
+INNER JOIN `album_skladba` ON `album_skladba`.`id_album` = `album`.`id_album`
+INNER JOIN `skladba` ON `skladba`.`id_skladba` = `album_skladba`.`id_skladba`
+INNER JOIN `album_interpret` ON `album_interpret`.`id_album` = `album`.`id_album`
+INNER JOIN `interpret` ON `interpret`.`id_interpret` = `album_interpret`.`id_interpret`
 WHERE
-skladba.delka = (SELECT MAX(delka) as delka FROM skladba)
+`skladba`.`delka` = (SELECT MAX(`delka`) as `delka` FROM `skladba`)
--- queries/all_albums.sql
+++ queries/all_albums.sql
@@ -4,16 +4,16 @@
 -- doplneny indexy na interpret.nazev a album.nazev
 
 SELECT
-interpret.nazev AS nazev_interpreta,
-album.nazev AS nazev_alba,
-album.datum_vydani,
-COUNT(album_skladba.id_album_skladba) as pocet_skladeb
-FROM album
-INNER JOIN album_skladba ON album_skladba.id_album=album.id_album
-INNER JOIN album_interpret ON album_interpret.id_album=album.id_album
-INNER JOIN interpret ON interpret.id_interpret=album_interpret.id_interpret
+`interpret`.`nazev` AS `nazev_interpreta`,
+`album`.`nazev` AS `nazev_alba`,
+`album`.`datum_vydani`,
+COUNT(`album_skladba`.`id_album_skladba`) AS `pocet_skladeb`
+FROM `album`
+INNER JOIN `album_skladba` ON `album_skladba`.`id_album` = `album`.`id_album`
+INNER JOIN `album_interpret` ON `album_interpret`.`id_album` = `album`.`id_album`
+INNER JOIN `interpret` ON `interpret`.`id_interpret` = `album_interpret`.`id_interpret`
 GROUP BY
-album.id_album
+`album`.`id_album`
 ORDER BY
-interpret.nazev ASC,
-album.nazev ASC
+`interpret`.`nazev` ASC,
+`album`.`nazev` ASC
